84af3d9dc39deda5605ec82442b685e8ff4e9c79
Angus Foreman <59096860+angusforeman@users.noreply.github.com>
2023-03-31 14:39:30 +0100

    removed blank Lines

--- single_tech_samples/datafactory/sample1_cicd/deploy.sh
+++ single_tech_samples/datafactory/sample1_cicd/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Access granted under MIT Open Source License: https://en.wikipedia.org/wiki/MIT_License
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
@@ -15,15 +14,12 @@
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
 # CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
-
 set -o errexit
 set -o pipefail
 set -o nounset
-
 . ./scripts/common.sh
 . ./scripts/verify_prerequisites.sh
 . ./scripts/init_environment.sh
-
 ###################
 # DEPLOY ALL RESOURCES FOR EACH ENVIRONMENT
 
